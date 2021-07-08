c5bab8b72919aa6c44068dd2334d8cd0ac9dc4ca
duttakapil <kapildutta28@gmail.com>
2021-07-08 14:26:53 +0530

    Trying to publish to GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -20,14 +20,14 @@ module.exports = {
     },
     resolve: { extensions : ["*", ".js", ".jsx"] },
     output : {
-        path : path.resolve(__dirname, "dist/"),
-        publicPath : "/dist/",
+        path : path.resolve(__dirname, "build/"),
+        publicPath : "/build/",
         filename : "bundle.js"
     },
     devServer : {
         contentBase : path.join(__dirname, "public/"),
         port : 3000,
-        publicPath : "http://localhost:3000/dist/",
+        publicPath : "http://localhost:3000/build/",
         hotOnly : true
     },
     plugins : [ new webpack.HotModuleReplacementPlugin() ]
